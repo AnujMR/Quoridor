cc9a073043ab316ce5534f4c16b41a6a61dbe301
AnujMR <anujramane22@gmail.com>
2026-03-12 18:37:17 +0530

    App.jsx issue fixed

--- quoridor_frontend/src/main.jsx
+++ quoridor_frontend/src/main.jsx
@@ -1,9 +1,7 @@
-import { StrictMode } from 'react'
 import './index.css'
-import Login from './screens/loginPage.jsx'
-import QuoridorBoard from './screens/quoridorBoard.jsx'
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
