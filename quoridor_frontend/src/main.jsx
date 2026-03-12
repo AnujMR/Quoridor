927141337b416aabf20f373e9b6f3d3f7723a9c6
AnujMR <anujramane22@gmail.com>
2026-03-12 14:45:50 +0530

    Router Dom added

--- quoridor_frontend/src/main.jsx
+++ quoridor_frontend/src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import Login from './screens/loginPage.jsx'
+import QuoridorBoard from './screens/quoridorBoard.jsx'
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    {/* <App /> */}
     {/* <QuoridorBoard /> */}
     <Login></Login>
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
